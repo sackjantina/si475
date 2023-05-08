a23e9a2dd51e0d75866b7a26ef9332c939b611de
Jack August Santina <m235580@usna.edu>
2023-05-08 10:58:35 -0400

    last update

--- competition/comp_ws/src/competition/competition/competition.py
+++ competition/comp_ws/src/competition/competition/competition.py
@@ -18,6 +18,8 @@ import numpy as np
 class Competition(Node):
     def __init__(self):
         super().__init__("Competition")
+
+        self.initialized = False
         
         # subscriber to read the wheel ticks 
         # self.subscriber = self.create_subscription(WheelTicks, '/wheel_ticks', self.tick_callback, qos_profile_sensor_data)
@@ -31,12 +33,18 @@ class Competition(Node):
         self.create_subscription(BatteryState, '/battery_state', self.get_battery, qos_profile_sensor_data)
         self.battery_percent = 0.0
 
-        self.time_interval = 0.5
+        # After setting the initial pose, from the command line run: ros2 topic pub -1 /start_navigation std_msgs/String
+        self.create_subscription(String, '/start_navigation', self.navigate, qos_profile_system_default) 
+        
+        self.create_subscription(String, '/return_home', self.go_home, qos_profile_system_default) 
+        self.create_subscription(String, '/leave_home', self.go_home, qos_profile_system_default) 
+
+
+        self.time_interval = 5
         self.create_timer(self.time_interval, self.state_machine)
 
         # State Functions
         self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
-
 
         self.bridge = CvBridge() # create a CvBridge object for later use
 
@@ -48,7 +56,7 @@ class Competition(Node):
         
         # Filter by Area.
         params.filterByArea = True
-        params.minArea = 100
+        params.minArea = 500
         
         # Filter by Circularity
         params.filterByCircularity = True
@@ -56,7 +64,7 @@ class Competition(Node):
         
         # Filter by Convexity
         params.filterByConvexity = True
-        params.minConvexity = 0.5
+        params.minConvexity = 0.2
         
         # Filter by Inertia
         params.filterByInertia = True
@@ -68,17 +76,12 @@ class Competition(Node):
 
         self.state = "Searching"
 
-        self.return_home = False
-        self.home = self.get_PoseStamped(0.0, 0.0, 0.0)
-
         self.navigator = BasicNavigator() 
         self.navigator.waitUntilNav2Active()
 
-        self.create_subscription(String, '/start_navigation', self.navigate, qos_profile_system_default) 
+        self.return_home = False
+        self.home = self.get_PoseStamped(-3.25, -0.405, -1.285)
 
-
-    def navigate(self, msg):
-        self.return_home = True
 
     def get_PoseStamped(self, x, y, angle):
         pose = PoseStamped()
@@ -94,12 +97,90 @@ class Competition(Node):
         return pose
 
     def process_image(self, msg):
-        # self.get_logger().error("Processing Image")
+
+        # img = self.bridge.imgmsg_to_cv2(msg)
+
+        # # path to the weights and model files
+        # weights = "ssd_mobilenet/frozen_inference_graph.pb"
+        # model = "ssd_mobilenet/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+        # # load the MobileNet SSD model trained  on the COCO dataset
+        # net = cv2.dnn.readNetFromTensorflow(weights, model)
+
+        # # load the class labels the model was trained on
+        # class_names = []
+        # with open("ssd_mobilenet/coco_names.txt", "r") as f:
+        #     class_names = f.read().strip().split("\n")
+
+        # # create a list of random colors to represent each class
+        # np.random.seed(42)
+        # colors = np.random.randint(0, 255, size=(len(class_names), 3))
+
+        # # loop over the frames
+        # while True:
+        #     # starter time to computer the fps
+        #     start = datetime.datetime.now()
+
+        #     # create a blob from the frame
+        #     blob = cv2.dnn.blobFromImage(
+        #         img, 1.0/127.5, (320, 320), [127.5, 127.5, 127.5])
+        #     # pass the blog through our network and get the output predictions
+        #     net.setInput(blob)
+        #     output = net.forward() # shape: (1, 1, 100, 7)
+
+        #     # loop over the number of detected objects
+        #     for detection in output[0, 0, :, :]: # output[0, 0, :, :] has a shape of: (100, 7)
+        #         # the confidence of the model regarding the detected object
+        #         probability = detection[2]
+        #         # if the confidence of the model is lower than 50%,
+        #         # we do nothing (continue looping)
+        #         if probability < 0.5:
+        #             continue
+
+        #         # extract the ID of the detected object to get
+        #         # its name and the color associated with it
+        #         class_id = int(detection[1])
+        #         label = class_names[class_id - 1].upper()
+        #         color = colors[class_id]
+        #         B, G, R = int(color[0]), int(color[1]), int(color[2])
+        #         # perform element-wise multiplication to get
+        #         # the (x, y) coordinates of the bounding box
+        #         box = [int(a * b) for a, b in zip(detection[3:7], [w, h, w, h])]
+        #         box = tuple(box)
+        #         # draw the bounding box of the object
+        #         cv2.rectangle(frame, box[:2], box[2:], (B, G, R), thickness=2)
+
+        #         # draw the name of the predicted object along with the probability
+        #         text = f"{label} {probability * 100:.2f}%"
+        #         cv2.putText(frame, text, (box[0], box[1]),
+        #                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+        #     # end time to compute the fps
+        #     end = datetime.datetime.now()
+        #     # calculate the frame per second and draw it on the frame
+        #     fps = f"FPS: {1 / (end - start).total_seconds():.2f}"
+        #     cv2.putText(frame, fps, (50, 50),
+        #                 cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 8)
+        #     cv2.imshow("Output", frame)
+        #     # write the frame to disk
+        #     writer.write(frame)
+        #     if cv2.waitKey(10) == ord("q"):
+        #         break
+
+        # # release the video capture, video writer, and close all windows
+        # video_cap.release()
+        # writer.release()
+        # cv2.destroyAllWindows()
+
+        
+
+        # # Show image
+        # cv2.imshow("Blob detection", frame)
+        # cv2.waitKey(100)
 
         img = self.bridge.imgmsg_to_cv2(msg)
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         hue, saturation, value = cv2.split(hsv[:,:])
-        ret, thresh = cv2.threshold(saturation, np.average(saturation)+ 3*np.std(saturation), 255, cv2.THRESH_BINARY)
+        ret, thresh = cv2.threshold(saturation, np.average(saturation)+ 2*np.std(saturation), 255, cv2.THRESH_BINARY)
 
         mask = cv2.erode(thresh, None, iterations=4)
         mask = cv2.dilate(mask, None, iterations=4)
@@ -108,8 +189,7 @@ class Competition(Node):
         keypoints = self.detector.detect(mask)
         blob_img = cv2.drawKeypoints(mask, keypoints, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         
-        h,w,c = blob_img.shape
-        middle = w/2
+        self.imheight, self.imwidth, _ = blob_img.shape
 
         if len(keypoints) > 0:
             for k in keypoints:
@@ -128,18 +208,54 @@ class Competition(Node):
         cv2.imshow('blobbed image', blob_img)
         cv2.waitKey(100)
 
+
         self.blobs = keypoints
 
     def get_battery(self, msg):
         self.battery_percent = msg.percentage
 
+    def navigate(self, msg):
+        # self.return_home = True
+        self.initialized = True
+
+    def go_home(self, msg):
+        self.return_home = True
+
+    def leave_home(self, msg):
+        self.return_home = False
+
+    def patrol(self):
+        path = []
+        poses = [ 
+            [0.25, -3.54, 0.23],
+            [3.46, -2.85, 1.75],
+            [3.07, -0.57, -2.87],
+            [-0.295, -1.03, -1.34]
+         ]
+        
+        for pose in poses:
+            p = self.get_PoseStamped(pose[0], pose[1], pose[2])
+            path.append(p)
+
+        # Start navigation
+        self.navigator.goThroughPoses(path)
+
     def state_machine(self):
-        self.get_logger().error("running state machine")
+        if not self.initialized:
+            return
+
+        # self.get_logger().error("running state machine")
+        # print("return home:", self.return_home)
 
         # check battery level
         if self.battery_percent < 0.2 or self.return_home:
             self.state = "Return"
             self.get_logger().error(self.state)
+
+            # cancel patrol task
+            if not self.navigator.isTaskComplete():
+                self.navigator.cancelTask()
+
             self.navigator.goToPose(self.home)
             return
 
@@ -149,9 +265,37 @@ class Competition(Node):
         if len(self.blobs) > 0:
             self.state = "Hunting"
             self.get_logger().error(self.state)
+
+            # cancel patrol task
+            if not self.navigator.isTaskComplete():
+                self.navigator.cancelTask()
+
+            balloon = self.blobs[0]
+            offset = -1*(balloon.pt[0] - (self.imwidth//2)) / (self.imwidth*1.5)
+
+            cmd = Twist()
+            cmd.linear.x = 0.1
+            cmd.linear.y = 0.0
+            cmd.linear.z = 0.0
+            cmd.angular.x = 0.0
+            cmd.angular.y = 0.0
+            cmd.angular.z = offset
+            self.publisher.publish(cmd)
+
         else:
             self.state = "Searching"
             self.get_logger().error(self.state)
+
+            if not self.navigator.isTaskComplete():
+                    self.navigator.cancelTask()
+
+            self.patrol()
+
+            # cmd = Twist()
+            # cmd.linear.x = 0.0
+            # cmd.linear.y = 0.0
+            # cmd.linear.z = 0.0
+            # self.publisher.publish(cmd)
         
 
 
--- competition/comp_ws/src/competition/setup.py
+++ competition/comp_ws/src/competition/setup.py
@@ -1,4 +1,6 @@
+import os
 from setuptools import setup
+from glob import glob
 
 package_name = 'competition'
 
@@ -10,6 +12,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # launch files
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
+        (os.path.join('share', package_name) + '/map', glob('map/*')),
+        (os.path.join('share', package_name) + '/config', glob('config/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
